19b7cae54138d07be91d5f35817579b9c40e6ee0
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2009-04-02 12:13:26 +0000

    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/public/puppet/modules/ganglia@2367 0e4a7186-e024-0410-a49b-aced43e0d94e

--- manifests/ganglia_monitor.pp
+++ manifests/ganglia_monitor.pp
@@ -1,38 +1,52 @@
 # $Id$
 
 class ganglia::monitor {
-  $ganglia_monitor_conf = $kernel ? {
-    "FreeBSD" => "/usr/local/etc/gmond.conf",
-      default => "/etc/ganglia/gmond.conf"
+  $ganglia_mconf_dir = $kernel ? {
+    "FreeBSD" => "/usr/local/etc",
+      default => "/etc/ganglia"
   }
-  $package = $kernel ? {
-    "FreeBSD" => "ganglia-monitor-core",
+  $ganglia_monitor_conf = "${ganglia_mconf_dir}/gmond.conf"
+    $package = $kernel ? {
+      "FreeBSD" => "ganglia-monitor-core",
+      "Darwin" => "ganglia-3.1.2-1.pkg.dmg",
       default => "ganglia-monitor",
-  }
+    }
   $service = $kernel ? {
     "FreeBSD" => "gmond",
-      "Darwin" => "com.42quarks.gmond",
-      default => "ganglia-monitor",
+    "Darwin" => "de.ikw.uos.gmond",
+    default => "ganglia-monitor",
   }
   $pathprefix = $kernel ? {
     "FreeBSD" => "/usr/local",
-      default => "/usr",
+    default => "/usr",
   } 
+  $run_as = $kernel ? {
+        "Darwin" => "nobody",
+        default => "ganglia",
+      }
   case $kernel {
     "Linux": {
       package{["libganglia1", "${package}"]:
 	ensure => "3.1.2-ikw-1",
-	       before => [ Service["${service}"], File["${ganglia_monitor_conf}"] ],
+	before => [ Service["${service}"], File["${ganglia_monitor_conf}"] ],
       }      
     }
     "Darwin": {
-      pkg_deploy{"ganglia_3.0.7.pkg.dmg": 
+      pkg_deploy{"${package}": 
 	before => [ Service["${service}"], File["${ganglia_monitor_conf}"] ],
       }
+      file{"/Library/LaunchDaemons/${service}.plist":
+	content => template("ganglia/${service}.plist.erb"),
+		require => Pkg_deploy["${package}"],            
+      }      
+      darwin_firewall{"any":
+      port => "8649",
+          }
     }
     default: {
 	       package{"${package}":
-		 before => [ Service["${service}"], File["${ganglia_monitor_conf}"] ],
+		 before => [ Service["${service}"], 
+			File["${ganglia_monitor_conf}"] ],
 	       }
 	     }
   }   
@@ -41,11 +55,31 @@ class ganglia::monitor {
 	   enable => "true",
 	   pattern => "gmond",
 	   subscribe => File["${ganglia_monitor_conf}"],
-	   require => Package["${package}"],
+	   require => $kernel ? {
+	     "Darwin" => Pkg_deploy["${package}"],
+	     default => Package["${package}"],
+	   }
   }
+  file{"${ganglia_mconf_dir}":
+    ensure => "directory",
+  }
+  file {"${ganglia_mconf_dir}/conf.d":
+    ensure => "directory",
+	   require => File["${ganglia_mconf_dir}"]
+  } 
+
   file{"${ganglia_monitor_conf}":
     content => template("ganglia/ganglia-monitor-conf.erb"),
-	    require => Package["${package}"],
+	    require => $kernel ? {
+	      "Darwin" => [ 
+		File["${ganglia_mconf_dir}"], 
+	      Pkg_deploy["${package}"] 
+		],
+	      default => [ 
+		File["${ganglia_mconf_dir}"],  
+	      Package["${package}"] 
+		],
+	    }
   }
   @@file{"${ganglia_metacollects}/meta-cluster-${fqdn}":
     tag => "ganglia_gmond_cluster_${ganglia_mcast_port}",
