4ee0f5bc701cca546e976553664f01a126499eb5
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2009-05-22 10:27:42 +0000

    more graph stuff and poseidon Heartbeat script
    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/public/puppet/modules/ganglia@2499 0e4a7186-e024-0410-a49b-aced43e0d94e

--- files/metrics-cron/poseidon2ganglia.sh
+++ files/metrics-cron/poseidon2ganglia.sh
@@ -7,7 +7,7 @@ SNMPGET="/usr/bin/snmpget -t 1 -r 5 -v 1 -c public sensor.ikw.Uni-Osnabrueck.DE:
 TEMPERATUR=$($SNMPGET .1.3.6.1.4.1.21796.3.3.3.1.6.1 |awk -F : '{print $4/10}')
 HUMIDITY=$($SNMPGET .1.3.6.1.4.1.21796.3.3.3.1.6.2 |awk -F : '{print $4/10}')
 DOOR=$($SNMPGET .1.3.6.1.4.1.21796.3.3.1.1.2.1 |awk -F : '{print $4}')
-echo "TEMP: $TEMPERATUR, HUM: $HUMIDITY, DOOR: $DOOR"
+[[ "x$DEBUG" != "x" ]] && echo "TEMP: $TEMPERATUR, HUM: $HUMIDITY, DOOR: $DOOR"
 /usr/bin/gmetric -S 131.173.33.117:sensor.ikw.Uni-Osnabrueck.DE -x 300 -n Temperature -v $TEMPERATUR -t float -u Celsius
 /usr/bin/gmetric -S 131.173.33.117:sensor.ikw.Uni-Osnabrueck.DE -x 300 -n Humidity -v $HUMIDITY -t float -u "% Rh"
 /usr/bin/gmetric -S 131.173.33.117:sensor.ikw.Uni-Osnabrueck.DE -x 300 -n Door -v $DOOR -t int8 -u State
---
+++ files/metrics-cron/poseidon2gangliaHeartbeat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# send heartbeat message to ganglia 
+#
+/usr/bin/gmetric -S 131.173.33.117:sensor.ikw.Uni-Osnabrueck.DE -H 
