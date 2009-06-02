7b026b7c1a1e360b3413efbcdf6e514c7b8df267
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2009-06-02 16:06:31 +0000

    fixes #545
    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/public/puppet/modules/ganglia@2544 0e4a7186-e024-0410-a49b-aced43e0d94e

---
+++ files/metrics-cron/packages_macos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# $Id$
+#
+# Ganglia metric for softwareupdates on mac os x
+
+STATEFILE="/var/tmp/packages_macos.state"
+#update if state does not exist, or if older than 7 days
+if [ ! -e ${STATEFILE} ] || [ $(stat -f %m -t %s ${STATEFILE}) -lt $(expr $(date '+%s') - 604800) ]; then
+  /usr/sbin/softwareupdate --list | grep -e '^[\ ]*\*' >${STATEFILE}
+fi
+
+PKGS=$(wc -l ${STATEFILE} | awk '{print $1}')
+gmetric --dmax=30000 --tmax=3600 --type=uint8 --name="Upgradeable Packages MacOSX" --value=${PKGS}
