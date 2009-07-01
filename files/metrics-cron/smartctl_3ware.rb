7cef6fde13c7e2be50c566ba1eb1d2c9da58b117
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2009-07-01 16:58:37 +0000

    fixed a but, where NOT-PRESENT drives were still reported
    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/public/puppet/modules/ganglia@2626 0e4a7186-e024-0410-a49b-aced43e0d94e

--- files/metrics-cron/smartctl_3ware.rb
+++ files/metrics-cron/smartctl_3ware.rb
@@ -9,11 +9,13 @@ if smartctl != "" and tw_cli != ""
   da=0
 %x{ls /dev/tw*}.chomp.split(/\n/).each{ |tw|
   controller=tw.match(/[0-9]+/)[0]
-  numdevs = %x{tw_cli /c#{controller} show |grep -e '^p.'|wc -l}.chomp.to_i
-  (numdevs-1).to_i.downto(0).each { |dev|
-    temp = %x{#{smartctl} -d 3ware,#{dev} -A #{tw}|grep -v Airflow |grep Temperature}.chomp.split(" ")[9]
+  numdevs = %x{tw_cli /c#{controller} show |grep -e '^p.'|grep -v NOT-PRESENT |wc -l}.chomp.to_i
+  if numdevs > 0 
+    (numdevs-1).to_i.downto(0).each { |dev|
+      temp = %x{#{smartctl} -d 3ware,#{dev} -A #{tw}|grep -v Airflow |grep Temperature}.chomp.split(" ")[9]
     %x{#{gmetric} --dmax=30000 --tmax=1800 --units="degrees C" --name="HDDTemp da#{da}" --value=#{temp} --type=uint8}
     da = da.next
-  }
+    }
+  end
 }
 end
