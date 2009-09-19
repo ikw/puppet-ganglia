247f790211903d5b64e3246ba47218796d15ec79
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2009-09-19 19:01:48 +0000

    refs #262, added metric
    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/public/puppet/modules/ganglia@2871 0e4a7186-e024-0410-a49b-aced43e0d94e

---
+++ files/metrics-cron/sensors_freebsd.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+# $Id$
+gmetric = %x{which gmetric}.chomp
+exit 0 if $? != 0
+
+sens = %x{sysctl dev.cpu|grep temparature}
+sens.each { |line|
+  mtch = line.match(/dev.cpu.(\d).*:\s(\d+)/)
+  %x{#{gmetric} --dmax=36000 --name="Sensors CPU #{mtch[1]}" --value="#{mtch[2]}" --type=float --tmax=300 --units="degrees C"}
+}
