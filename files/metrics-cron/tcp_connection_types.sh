191fb4af124ddc03b45e492d5388c67615c61b87
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2009-09-22 11:58:33 +0000

    tcp connection statistics
    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/public/puppet/modules/ganglia@2879 0e4a7186-e024-0410-a49b-aced43e0d94e

---
+++ files/metrics-cron/tcp_connection_types.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+CLIENT=`which gmetric`
+[[ -x $CLIENT ]] || exit 0
+NETSTAT=`/bin/netstat -t -n`
+VALUE=$(echo "$NETSTAT"|egrep "ESTABLISHED"|wc -l)
+$CLIENT --dmax=30000 --tmax=300 --units=Connections -t uint16 --slope=positive -n TCP_ESTABLISHED -v $VALUE
+VALUE=$(echo "$NETSTAT"|egrep "CLOSE_WAIT"|wc -l)
+$CLIENT --dmax=30000 --tmax=300 --units=Connections -t uint16 --slope=positive -n TCP_CLOSE_WAIT -v $VALUE
+VALUE=$(echo "$NETSTAT"|egrep "TIME_WAIT"|wc -l)
+$CLIENT --dmax=30000 --tmax=300 --units=Connections -t uint16 --slope=positive -n TCP_TIME_WAIT -v $VALUE
