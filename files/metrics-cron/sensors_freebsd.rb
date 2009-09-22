ae1fce77a6bf7423f2a704595998ccaa73358338
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2009-09-22 01:32:37 +0000

    some sensor additions and cleanups
    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/public/puppet/modules/ganglia@2874 0e4a7186-e024-0410-a49b-aced43e0d94e

--- files/metrics-cron/sensors_freebsd.rb
+++ files/metrics-cron/sensors_freebsd.rb
@@ -3,7 +3,7 @@
 gmetric = %x{which gmetric}.chomp
 exit 0 if $? != 0
 
-sens = %x{sysctl dev.cpu|grep temparature}
+sens = %x{sysctl dev.cpu|grep temperature}
 sens.each { |line|
   mtch = line.match(/dev.cpu.(\d).*:\s(\d+)/)
   %x{#{gmetric} --dmax=36000 --name="Sensors CPU #{mtch[1]}" --value="#{mtch[2]}" --type=float --tmax=300 --units="degrees C"}
--- files/metrics-cron/smartctl_3ware.rb
+++ files/metrics-cron/smartctl_3ware.rb
@@ -5,17 +5,30 @@ tw_cli = %x{which tw_cli}.chomp
 gmetric = %x{which gmetric}.chomp
 exit 0 if $? != 0
 
+gmetric = "#{gmetric} --dmax=30000 --tmax=1800 --units=\"degrees C\" --type=uint16 --slope=positive"
 if smartctl != "" and tw_cli != ""
-  da=0
-%x{ls /dev/tw*}.chomp.split(/\n/).each{ |tw|
-  controller=tw.match(/[0-9]+/)[0]
-  numdevs = %x{tw_cli /c#{controller} show |grep -e '^p.'|grep -v NOT-PRESENT |wc -l}.chomp.to_i
-  if numdevs > 0 
-    (numdevs-1).to_i.downto(0).each { |dev|
-      temp = %x{#{smartctl} -d 3ware,#{dev} -A #{tw}|grep -v Airflow |grep Temperature}.chomp.split(" ")[9]
-    %x{#{gmetric} --dmax=30000 --tmax=1800 --units="degrees C" --name="HDDTemp da#{da}" --value=#{temp} --type=uint8}
-    da = da.next
-    }
-  end
-}
+  
+  %x{ls /dev/tw*}.chomp.split(/\n/).each{ |tw|
+    da=0
+    controller=tw.match(/[0-9]+/)[0]
+    numdevs = %x{tw_cli /c#{controller} show 2>/dev/null |grep -e '^p.'|grep -v NOT-PRESENT |wc -l}.chomp.to_i
+    if numdevs > 0
+      (numdevs-1).to_i.downto(0).each { |dev|
+        #temp = %x{#{smartctl} -d 3ware,#{dev} -A #{tw}|grep -v Airflow |grep Temperature}.chomp.split(" ")[9]
+        %x{#{smartctl} -d 3ware,#{dev} -A #{tw}}.chomp.each { |line|
+          vals = line.split(" ")
+          if line =~ /Temperature_Celsius/
+            %x{#{gmetric} --name="Sensors HDD Temp tw_c#{dev}_p#{da}" --value=#{vals[9]}}
+          elsif line =~ /Current_Pending_Sector/
+            %x{#{gmetric} --name="Sensors HDD Current Pending Sector tw_c#{dev}_p#{da}" --value=#{vals[9]}}
+          elsif line =~ /Offline_Uncorrectable/
+            %x{#{gmetric} --name="Sensors HDD Offline Uncorrectable tw_c#{dev}_p#{da}" --value=#{vals[9]}}
+          elsif line =~ /UDMA_CRC_Error_Count/
+            %x{#{gmetric} --name="Sensors HDD UDMA CRC Error tw_c#{dev}_p#{da}" --value=#{vals[9]}}
+          end
+        }
+        da = da.next
+      }
+    end
+  }
 end
