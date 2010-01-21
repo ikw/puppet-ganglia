3b4836146a034de10a769f060e29841786b112a1
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2010-01-21 11:25:23 +0000

    indentation
    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/public/puppet/modules/ganglia@3129 0e4a7186-e024-0410-a49b-aced43e0d94e

--- files/metrics-cron/smartctl.rb
+++ files/metrics-cron/smartctl.rb
@@ -28,31 +28,31 @@ exit 0 if $? != 0
 debug = ARGV[1] == "debug" ? true : false;
 drives = Facter.value("harddrives_smartcaps")
 if drives.nil?
-    exit 0
+  exit 0
 end
 drives = drives.split(",")
 
 gmetric = "#{gmetric} --dmax=30000 --tmax=1800 --type=int16"
 
 if smartctl != "" && drives.length > 0
-    drives.each{ |dev|
-        %x{#{smartctl} -A /dev/#{dev}}.chomp.each { |line|
-            vals = line.split(" ")
-            if line =~ /Temperature_Celsius/
-                puts "#{gmetric} --units=\"degrees C\" --name=\"Sensors HDD Temp #{dev}\" --value=#{vals[9]}" if debug
-                %x{#{gmetric} --units="degrees C" --name="Sensors HDD Temp #{dev}" --value=#{vals[9]}}
-            elsif line =~ /Current_Pending_Sector/
-                puts "#{gmetric} --units=\"Number\" --name=\"Sensors HDD Current Pending Sector #{dev}\" --value=#{vals[9]}" if debug
-                %x{#{gmetric} --units="Number" --name="Sensors HDD Current Pending Sector #{dev}" --value=#{vals[9]}}
-            elsif line =~ /Offline_Uncorrectable/
-                puts "#{gmetric} --units=\"Number\" --name=\"Sensors HDD Offline Uncorrectable #{dev}\" --value=#{vals[9]}" if debug
-                %x{#{gmetric} --units="Number" --name="Sensors HDD Offline Uncorrectable #{dev}" --value=#{vals[9]}}
-            elsif line =~ /UDMA_CRC_Error_Count/
-                puts "#{gmetric} --units=\"Number\" --name=\"Sensors HDD UDMA CRC Error #{dev}\" --value=#{vals[9]}" if debug
-                %x{#{gmetric} --units="Number" --name="Sensors HDD UDMA CRC Error #{dev}" --value=#{vals[9]}}
-            end
-        }
+  drives.each{ |dev|
+    %x{#{smartctl} -A /dev/#{dev}}.chomp.each { |line|
+      vals = line.split(" ")
+      if line =~ /Temperature_Celsius/
+        puts "#{gmetric} --units=\"degrees C\" --name=\"Sensors HDD Temp #{dev}\" --value=#{vals[9]}" if debug
+        %x{#{gmetric} --units="degrees C" --name="Sensors HDD Temp #{dev}" --value=#{vals[9]}}
+      elsif line =~ /Current_Pending_Sector/
+        puts "#{gmetric} --units=\"Number\" --name=\"Sensors HDD Current Pending Sector #{dev}\" --value=#{vals[9]}" if debug
+        %x{#{gmetric} --units="Number" --name="Sensors HDD Current Pending Sector #{dev}" --value=#{vals[9]}}
+      elsif line =~ /Offline_Uncorrectable/
+        puts "#{gmetric} --units=\"Number\" --name=\"Sensors HDD Offline Uncorrectable #{dev}\" --value=#{vals[9]}" if debug
+        %x{#{gmetric} --units="Number" --name="Sensors HDD Offline Uncorrectable #{dev}" --value=#{vals[9]}}
+      elsif line =~ /UDMA_CRC_Error_Count/
+        puts "#{gmetric} --units=\"Number\" --name=\"Sensors HDD UDMA CRC Error #{dev}\" --value=#{vals[9]}" if debug
+        %x{#{gmetric} --units="Number" --name="Sensors HDD UDMA CRC Error #{dev}" --value=#{vals[9]}}
+      end
     }
+  }
 end
 ## remove pid
 FileUtils.remove(pidfile)
