f8e0b3864400781932b341716b96d2d8ead1ba5d
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2010-02-25 13:57:28 +0000

    refs #1136, init.d update
    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/public/puppet/modules/ganglia@3179 0e4a7186-e024-0410-a49b-aced43e0d94e

--- files/ganglia-webfrontend/conf.php
+++ files/ganglia-webfrontend/conf.php
@@ -18,7 +18,7 @@ $template_name = "default";
 #
 
 # Where gmetad stores the rrd archives.
-$gmetad_root = "/mnt/ganglia";
+$gmetad_root = "/var/lib/ganglia";
 $rrds = "$gmetad_root/rrds";
 
 # Leave this alone if rrdtool is installed in $gmetad_root,
