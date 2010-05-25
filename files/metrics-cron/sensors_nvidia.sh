1470aff726906334b7cd6c289a614fc19437953c
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2010-05-25 19:29:25 +0000

    refs #1384, handling of different sensor output
    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/public/puppet/modules/ganglia@3441 0e4a7186-e024-0410-a49b-aced43e0d94e

--- files/metrics-cron/sensors_nvidia.sh
+++ files/metrics-cron/sensors_nvidia.sh
@@ -13,7 +13,14 @@ GMETRIC="${GMETRIC} --dmax=30000 --slope=positive --tmax=1800"
 INF=$(nvclock -i)
 
 TEMP=$(echo "${INF}" | awk '/GPU temperature:/{sub(/C/,"",$3); print $3}')
+BTEMP=$(echo "${INF}" | awk '/Board temperature:/{sub(/C/,"",$3); print $3}')
 FAN=$(echo "${INF}" | awk '/Fanspeed:/{sub(/%/,"",$2); print $2}')
+if [ $(echo "${INF}" |grep Fanspeed: |grep -c RPM) -eq 1 ]; then
+		UNITS="RPM"
+else
+	UNITS="%"
+fi
 
 ${GMETRIC} --name="Sensors GPU Temp" --type=uint32 --units="degrees C" --value=${TEMP}
-${GMETRIC} --name="Sensors GPU Fanspeed" --type=float --units="%" --value=${FAN}
+[[ "X${BTEMP}" == "X" ]] || ${GMETRIC} --name="Sensors GPU Board Temp" --type=uint32 --units="degrees C" --value=${BTEMP}
+${GMETRIC} --name="Sensors GPU Fanspeed" --type=float --units=${UNITS} --value=${FAN}
