b286994d4e0835e92cd64ecfbab6dc8a1a4131f5
uwaechte <uwaechte@0e4a7186-e024-0410-a49b-aced43e0d94e>
2010-06-16 09:44:37 +0000

    expr error fix
    git-svn-id: https://svn.ikw.uni-osnabrueck.de/dav/ikwadmin/public/puppet/modules/ganglia@3474 0e4a7186-e024-0410-a49b-aced43e0d94e

--- files/run-metrics.sh
+++ files/run-metrics.sh
@@ -6,6 +6,7 @@
 # It runs all scripts in a given directory.
 #
 ## run gmetrics from directory $1
+export USER=root
 . /etc/profile
 for I in $(ls $1/* 2>/dev/null); do
 	[[ -x $I ]] && $I 2>/dev/null 
